24c662eddf226beaf489dc0336fe2bb2a4c37764
pratiksha <pratu@gmail.com>
2024-12-30 13:56:21 +0530

    QUESTION 1 completed

--- QUESTION1/INPUT/script.js
+++ QUESTION1/INPUT/script.js
@@ -1,14 +1,12 @@
-function outerFunction(outerParam){
-    //variable declared within the function
-    let outerVar="Hello I am variable from OuterFunction";
-    return function innerFunction(innerParam){
-        //Accessing variables from innerFunction and outerFunction
-        console.log("Outer Parameter",outerParam);
-        console.log("Outer Function Variable",outerVar);
-        console.log("Inner Parameter",innerParam);
+function outerFunction(outerParam) {
+    let outerVar = "Hello I am variable from OuterFunction";
+    return function innerFunction(innerParam) {
+        console.log("Outer Parameter:", outerParam);
+        console.log("Outer Function Variable:", outerVar);
+        console.log("Inner Parameter:", innerParam);
     };
 }
-//create reference to inner function
-let innerReference=outerFunction("Hello from Outer Parameter");
-//call the inner function with an argument
-innerFunction("Hello From Inner Parameter");
+
+let innerReference = outerFunction("Hello from Outer Parameter");
+
+innerReference("Hello From Inner Parameter");
